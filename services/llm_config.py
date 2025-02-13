eb617b22fb69c4e598155d3cacd17ea468471116
Ivisson Pereira <ivipnascimento@hotmail.com>
2025-02-13 17:39:08 -0300

    CONFIG: reduce of max_new_tokens and

--- services/llm_config.py
+++ services/llm_config.py
@@ -1,7 +1,7 @@
 MODEL_ID="ibm-granite/granite-3.1-2b-instruct"
 TASK="text-generation"
 MODEL_PARAMS={
-    "max_new_tokens": 1024,
+    "max_new_tokens": 50,
     "top_k": 50,
     "temperature":0,
     "top_p":200,
